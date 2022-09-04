70d553068106fcf9ff2e2c4cb5996d80daa73877
langermank <langermank@gmail.com>
2022-09-04 10:13:40 -0700

    cleanup css

--- .eleventy.js
+++ .eleventy.js
@@ -1,203 +1,217 @@
-const fs = require("fs");
-const yaml = require("js-yaml");
-const xmlFiltersPlugin = require("eleventy-xml-plugin");
-const { DateTime } = require("luxon");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
+const fs = require('fs')
+const yaml = require('js-yaml')
+const xmlFiltersPlugin = require('eleventy-xml-plugin')
+const { DateTime } = require('luxon')
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
 
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const pluginNavigation = require('@11ty/eleventy-navigation')
 
-const CleanCSS = require("clean-css");
+const CleanCSS = require('clean-css')
 
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addFilter('cssmin', function (code) {
+    return new CleanCSS({}).minify(code).styles
+  })
 
-    eleventyConfig.addFilter("cssmin", function(code) {
-        return new CleanCSS({}).minify(code).styles;
-    });
+  eleventyConfig.addFilter('values', Object.values)
 
-    eleventyConfig.addFilter('values', Object.values);
+  // copy static files to dist
+  eleventyConfig.addPassthroughCopy('src/static')
 
-    // copy static files to dist
-    eleventyConfig.addPassthroughCopy("src/static");
+  eleventyConfig.addWatchTarget('./src/static/css')
 
-    eleventyConfig.addWatchTarget("./src/static/css");
+  eleventyConfig.addPassthroughCopy({
+    'node_modules/open-props/normalize.min.css':
+      'static/css/open-props/normalize.css',
+  })
 
-    eleventyConfig.addPassthroughCopy({
-        "node_modules/open-props/normalize.min.css": "static/css/open-props/normalize.css"
-    });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/animations.min.css": "static/css/open-props/animations.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/animations.min.css": "static/css/open-props/animations.css"
-    // });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/borders.min.css": "static/css/open-props/borders.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/borders.min.css": "static/css/open-props/borders.css"
-    // });
+  eleventyConfig.addPassthroughCopy({
+    'node_modules/open-props/buttons.min.css':
+      'static/css/open-props/buttons.css',
+  })
 
-    eleventyConfig.addPassthroughCopy({
-        "node_modules/open-props/buttons.min.css": "static/css/open-props/buttons.css"
-    });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/colors-hsl.min.css": "static/css/open-props/colors-hsl.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/colors-hsl.min.css": "static/css/open-props/colors-hsl.css"
-    // });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/easings.min.css": "static/css/open-props/easings.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/easings.min.css": "static/css/open-props/easings.css"
-    // });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/fonts.min.css": "static/css/open-props/fonts.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/fonts.min.css": "static/css/open-props/fonts.css"
-    // });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/gradients.min.css": "static/css/open-props/gradients.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/gradients.min.css": "static/css/open-props/gradients.css"
-    // });
+  eleventyConfig.addPassthroughCopy({
+    'node_modules/open-props/media.min.css':
+      'static/css/open-props/media-node.css',
+  })
 
-    eleventyConfig.addPassthroughCopy({
-        "node_modules/open-props/media.min.css": "static/css/open-props/media-node.css"
-    });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/shadows.min.css": "static/css/open-props/shadows.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/shadows.min.css": "static/css/open-props/shadows.css"
-    // });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/sizes.min.css": "static/css/open-props/sizes.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/sizes.min.css": "static/css/open-props/sizes.css"
-    // });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/borders.min.css": "static/css/open-props/borders.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/borders.min.css": "static/css/open-props/borders.css"
-    // });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/zindex.min.css": "static/css/open-props/zindex.css"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/zindex.min.css": "static/css/open-props/zindex.css"
-    // });
+  // eleventyConfig.addPassthroughCopy({
+  //     "node_modules/open-props/src/extra/**/*.css": "static/css/open-props/"
+  // });
 
-    // eleventyConfig.addPassthroughCopy({
-    //     "node_modules/open-props/src/extra/**/*.css": "static/css/open-props/"
-    // });
+  eleventyConfig.addPlugin(pluginNavigation)
 
-    eleventyConfig.addPlugin(pluginNavigation);
+  // Make "_data" capable of reading .yaml files
+  eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents))
 
-    // Make "_data" capable of reading .yaml files
-    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  // filters
+  // eleventyConfig.addFilter('postDate', (dateObj) => {
+  //   return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  // })
 
-    // filters
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    });
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd LLL yyyy')
+  })
 
-    eleventyConfig.addFilter("readableDate", dateObj => {
-        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-    });
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'America/Chicago',
+    }).toLocaleString(DateTime.DATE_MED)
+  })
 
-    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-    });
+  eleventyConfig.addFilter('formatDate', (date) => {
+    const d = DateTime.fromISO(date)
+    return `${d.toFormat('LLL')} ${d.toFormat('d')}, ${d.toFormat('yyyy')}`
+  })
 
+  eleventyConfig.addFilter('pubDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'America/Chicago',
+    }).toHTTP()
+  })
 
-    // Add a "get audio duration" function to template languages (broken)
-    //   eleventyConfig.addShortcode(
-    //     "getAudioDurationByPath",
-    //     require("./src/shortcodes/getAudioDurationByPath.js")
-    //   );
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd')
+  })
 
-    // Add a "get file size" function to template languages
-    eleventyConfig.addFilter(
-        "getFileSizeByPath", require("./src/filters/getFileSizeByPath.js")
-    );
+  // Add a "get audio duration" function to template languages (broken)
+  //   eleventyConfig.addShortcode(
+  //     "getAudioDurationByPath",
+  //     require("./src/shortcodes/getAudioDurationByPath.js")
+  //   );
 
-    // Get the first `n` elements of a collection.
-    eleventyConfig.addFilter("head", (array, n) => {
-        if(!Array.isArray(array) || array.length === 0) {
-            return [];
-        }
-        if( n < 0 ) {
-            return array.slice(n);
-        }
+  // Add a "get file size" function to template languages
+  eleventyConfig.addFilter(
+    'getFileSizeByPath',
+    require('./src/filters/getFileSizeByPath.js')
+  )
 
-        return array.slice(0, n);
-    });
-
-    // Return the smallest number argument
-    eleventyConfig.addFilter("min", (...numbers) => {
-        return Math.min.apply(null, numbers);
-    });
-
-    function filterTagList(tags) {
-        return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (!Array.isArray(array) || array.length === 0) {
+      return []
+    }
+    if (n < 0) {
+      return array.slice(n)
     }
 
-    eleventyConfig.addFilter("filterTagList", filterTagList)
+    return array.slice(0, n)
+  })
 
-    // Create an array of all tags
-    eleventyConfig.addCollection("tagList", function(collection) {
-        let tagSet = new Set();
-        collection.getAll().forEach(item => {
-        (item.data.tags || []).forEach(tag => tagSet.add(tag));
-        });
+  // Return the smallest number argument
+  eleventyConfig.addFilter('min', (...numbers) => {
+    return Math.min.apply(null, numbers)
+  })
 
-        return filterTagList([...tagSet]);
-    });
+  function filterTagList(tags) {
+    return (tags || []).filter(
+      (tag) => ['all', 'nav', 'post', 'posts'].indexOf(tag) === -1
+    )
+  }
 
-    // eleventyConfig.addCollection('episodes', curry(addFilteredCollection)(['episodes/*.md'], compareDatesDesc));
+  eleventyConfig.addFilter('filterTagList', filterTagList)
 
+  // Create an array of all tags
+  eleventyConfig.addCollection('tagList', function (collection) {
+    let tagSet = new Set()
+    collection.getAll().forEach((item) => {
+      ;(item.data.tags || []).forEach((tag) => tagSet.add(tag))
+    })
 
-    // shortcodes
-    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+    return filterTagList([...tagSet])
+  })
 
-    // Customize Markdown library and settings:
+  // eleventyConfig.addCollection('episodes', curry(addFilteredCollection)(['episodes/*.md'], compareDatesDesc));
+
+  // shortcodes
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`)
+
+  // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
     html: true,
-    linkify: true
+    linkify: true,
   }).use(markdownItAnchor, {
     permalink: markdownItAnchor.permalink.ariaHidden({
-      placement: "after",
-      class: "direct-link",
-      symbol: "#"
+      placement: 'after',
+      class: 'direct-link',
+      symbol: '#',
     }),
-    level: [1,2,3,4],
-    slugify: eleventyConfig.getFilter("slugify")
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+    level: [1, 2, 3, 4],
+    slugify: eleventyConfig.getFilter('slugify'),
+  })
+  eleventyConfig.setLibrary('md', markdownLibrary)
 
   // Override Browsersync defaults (used only with --serve)
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync('_site/404.html')
 
-        browserSync.addMiddleware("*", (req, res) => {
+        browserSync.addMiddleware('*', (req, res) => {
           // Provides the 404 content without redirect.
-          res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
-          res.write(content_404);
-          res.end();
-        });
+          res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' })
+          res.write(content_404)
+          res.end()
+        })
       },
     },
     ui: false,
-    ghostMode: false
-  });
+    ghostMode: false,
+  })
 
   return {
     // Control which files Eleventy will process
     // e.g.: *.md, *.njk, *.html, *.liquid
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-          "liquid",
-      "js"
-    ],
+    templateFormats: ['md', 'njk', 'html', 'liquid', 'js'],
 
     // Pre-process *.md files with: (default: `liquid`)
-    markdownTemplateEngine: "njk",
+    markdownTemplateEngine: 'njk',
 
     // Pre-process *.html files with: (default: `liquid`)
-    htmlTemplateEngine: "njk",
+    htmlTemplateEngine: 'njk',
 
     // -----------------------------------------------------------------
     // If your site deploys to a subdirectory, change `pathPrefix`.
@@ -210,46 +224,44 @@ module.exports = (eleventyConfig) => {
     // You can also pass this in on the command line using `--pathprefix`
 
     // Optional (default is shown)
-    pathPrefix: "/",
+    pathPrefix: '/',
     // -----------------------------------------------------------------
 
     // These are all optional (defaults are shown):
     dir: {
-      input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "dist"
-    }
-  };
+      input: 'src',
+      includes: '_includes',
+      data: '_data',
+      output: 'dist',
+    },
+  }
 
+  //   // Make Liquid capable of rendering "partials"
+  //   eleventyConfig.setLiquidOptions({
+  //     dynamicPartials: true,
+  //     strict_filters: true,
+  //   });
 
-//   // Make Liquid capable of rendering "partials"
-//   eleventyConfig.setLiquidOptions({
-//     dynamicPartials: true,
-//     strict_filters: true,
-//   });
+  //   // Add Jekyll-like XML tools
+  //   eleventyConfig.addPlugin(xmlFiltersPlugin);
 
-//   // Add Jekyll-like XML tools
-//   eleventyConfig.addPlugin(xmlFiltersPlugin);
+  //   // Add Jekyll-like Markdownify
+  //   let md_options = {
+  //     html: true,
+  //     breaks: true,
+  //     linkify: true,
+  //   };
+  //   let md = require("markdown-it");
+  //   let markdownLib = md(md_options);
+  //   eleventyConfig.addPairedShortcode("markdown", (content) => {
+  //     return markdownLib.render(content);
+  //   });
 
-//   // Add Jekyll-like Markdownify
-//   let md_options = {
-//     html: true,
-//     breaks: true,
-//     linkify: true,
-//   };
-//   let md = require("markdown-it");
-//   let markdownLib = md(md_options);
-//   eleventyConfig.addPairedShortcode("markdown", (content) => {
-//     return markdownLib.render(content);
-//   });
-
-//   // Clarify which folder is for input and which folder is for output
-//   return {
-//     dir: {
-//       input: "src",
-//       output: "dist",
-//     },
-//   };
-
-};
+  //   // Clarify which folder is for input and which folder is for output
+  //   return {
+  //     dir: {
+  //       input: "src",
+  //       output: "dist",
+  //     },
+  //   };
+}
--- src/markdown/episodes/pilot.md
+++ src/markdown/episodes/pilot.md
@@ -1,12 +1,13 @@
 ---
 audio_file_path: './src/static/audio/uxatyourleisure-pilot.mp3'
 audio_file_relative_url: '/static/audio/uxatyourleisure-pilot.mp3'
-title: "UX At Your Leisure Pilot"
-subtitle: "Pilot episode"
-pubDate: "Sat, 13 August 2022 19:00:00 GMT"
-summary: "The women behind Ladies that UX Boston start a podcast"
+title: 'UX At Your Leisure Pilot'
+subtitle: 'Pilot episode'
+pubDate: 'Sat, 13 August 2022 19:00:00 GMT'
+date: '2022-08-13'
+summary: 'The women behind Ladies that UX Boston start a podcast'
 description: "<p>The women behind Ladies that UX Boston are starting a podcast! In this pilot episode, we discuss what its been like to organize a meetup during a pandemic, how we got into Ladies that UX, and where we see meetups going in the future.</p><p>Find us:</p><ul><li><a href='https://twitter.com/UXAtYourLeisure'>@UXAtYourLeisure</a></li><li>Katie&#58; <a href='https://twitter.com/langermank'>@langermank</a></li><li>Lara&#58; <a href='https://twitter.com/lar_greenberg'>@lar_greenberg</a></li><li>Olga&#58; <a href='https://twitter.com/olgavperfilieva'>@olgavperfilieva</a></li></ul>"
-postDescription: "The women behind Ladies that UX Boston are starting a podcast! In this pilot episode, we discuss what its been like to organize a meetup during a pandemic, how we got into Ladies that UX, and where we see meetups going in the future."
+postDescription: 'The women behind Ladies that UX Boston are starting a podcast! In this pilot episode, we discuss what its been like to organize a meetup during a pandemic, how we got into Ladies that UX, and where we see meetups going in the future.'
 explicit: false
 episode_number: 0
 season: 1
